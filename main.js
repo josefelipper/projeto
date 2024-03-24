bed938148e1c56c7de0d8f9f5365066864d0e276
José Felippe <158535233+josefelipper@users.noreply.github.com>
2024-03-24 19:11:28 -0300

    atualizações

---
+++ main.js
@@ -0,0 +1,14 @@
+ScrollReveal().reveal('.titulo', {
+    delay: 100,
+    distance: '30px',
+    duration: 1500,
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    interval: 0,
+    opacity: 0,
+    origin: 'right',
+    rotate: {
+        x: 0,
+        y: 0,
+        z: 0,
+    }
+})
